2e0fba82841af68242ae421a20f7c27c947fb81f
Jason Kiptoo <kiptoojason@gmail.com>
2023-04-18 17:04:52 +0300

    styled responsiveness

--- src/views/EventsDisplay.vue
+++ src/views/EventsDisplay.vue
@@ -3,27 +3,21 @@
     <div class="title">
       <h2 class="title">Popular events</h2>
     </div>
-  
-      <div
-        
-        class="events-container"
-      >
-        <div class="event-card" v-for="(event, index) in events"
-        :key="index">
-          <img :src="event.imageUrl" class="card-img" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">{{ event.eventName }}</h5>
-            <p class="card-text">{{ event.description }}</p>
-            <p class="card-text">Date: {{ event.date }}</p>
-            <p class="card-text">Time: {{ event.time }}</p>
-            <p class="card-text">
-              Available slots: {{ event.availableSlots }}
-            </p>
-            <a href="#" class="btn btn-primary">Book now</a>
-          </div>
+
+    <div class="events-container">
+      <div class="event-card" v-for="(event, index) in events" :key="index">
+        <img :src="event.imageUrl" class="card-img" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{ event.eventName }}</h5>
+          <p class="card-text">{{ event.description }}</p>
+          <p class="card-text">Date: {{ event.date }}</p>
+          <p class="card-text">Time: {{ event.time }}</p>
+          <p class="card-text">Available slots: {{ event.availableSlots }}</p>
+          <a href="#" class="btn btn-primary">Book now</a>
         </div>
       </div>
-</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,70 +25,76 @@ export default {
   data() {
     return {
       events: [
-      {
-  eventName: "Music Festival",
-  description:
-    "Join us for a weekend of live music from some of the best bands around!",
-  date: "2023-07-08",
-  time: "12:00 PM",
-  imageUrl: "https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-  availableSlots: 500,
-},
-{
-  eventName: "Charity Gala",
-  description:
-    "Support a good cause and enjoy an elegant evening of dinner, drinks, and entertainment.",
-  date: "2023-09-15",
-  time: "7:00 PM",
-  imageUrl: "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-  availableSlots: 100,
-},
-{
-  eventName: "Art Exhibition",
-  description:
-    "Explore the works of some of the most talented artists in the world.",
-  date: "2023-05-20",
-  time: "10:00 AM",
-  imageUrl: "https://plus.unsplash.com/premium_photo-1663012861364-db0fbb286145?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=777&q=80",
-  availableSlots: 200,
-},
-{
-  eventName: "Food Festival",
-  description:
-    "Sample the culinary delights of dozens of top chefs and restaurants.",
-  date: "2023-06-03",
-  time: "11:00 AM",
-  imageUrl: "https://images.unsplash.com/photo-1530023367847-a683933f4172?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
-  availableSlots: 300,
-},
-{
-  eventName: "Sports Tournament",
-  description:
-    "Watch some of the world's top athletes compete in a variety of sports.",
-  date: "2023-08-12",
-  time: "9:00 AM",
-  imageUrl: "https://images.unsplash.com/photo-1561912774-79769a0a0a7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-  availableSlots: 1000,
-},
-{
-  eventName: "Technology Conference",
-  description:
-    "Learn about the latest trends and innovations in the tech industry from industry experts.",
-  date: "2023-11-01",
-  time: "8:00 AM",
-  imageUrl: "https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-  availableSlots: 500,
-},
-{
-  eventName: "Fashion Show",
-  description:
-    "See the latest styles and trends from top designers and models.",
-  date: "2023-10-05",
-  time: "6:00 PM",
-  imageUrl: "https://images.unsplash.com/photo-1515169067868-5387ec356754?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-  availableSlots: 200,
-},
-
+        {
+          eventName: "Music Festival",
+          description:
+            "Join us for a weekend of live music from some of the best bands around!",
+          date: "2023-07-08",
+          time: "12:00 PM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+          availableSlots: 500,
+        },
+        {
+          eventName: "Charity Gala",
+          description:
+            "Support a good cause and enjoy an elegant evening of dinner, drinks, and entertainment.",
+          date: "2023-09-15",
+          time: "7:00 PM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          availableSlots: 100,
+        },
+        {
+          eventName: "Art Exhibition",
+          description:
+            "Explore the works of some of the most talented artists in the world.",
+          date: "2023-05-20",
+          time: "10:00 AM",
+          imageUrl:
+            "https://plus.unsplash.com/premium_photo-1663012861364-db0fbb286145?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=777&q=80",
+          availableSlots: 200,
+        },
+        {
+          eventName: "Food Festival",
+          description:
+            "Sample the culinary delights of dozens of top chefs and restaurants.",
+          date: "2023-06-03",
+          time: "11:00 AM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1530023367847-a683933f4172?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+          availableSlots: 300,
+        },
+        {
+          eventName: "Sports Tournament",
+          description:
+            "Watch some of the world's top athletes compete in a variety of sports.",
+          date: "2023-08-12",
+          time: "9:00 AM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1561912774-79769a0a0a7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+          availableSlots: 1000,
+        },
+        {
+          eventName: "Technology Conference",
+          description:
+            "Learn about the latest trends and innovations in the tech industry from industry experts.",
+          date: "2023-11-01",
+          time: "8:00 AM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          availableSlots: 500,
+        },
+        {
+          eventName: "Fashion Show",
+          description:
+            "See the latest styles and trends from top designers and models.",
+          date: "2023-10-05",
+          time: "6:00 PM",
+          imageUrl:
+            "https://images.unsplash.com/photo-1515169067868-5387ec356754?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          availableSlots: 200,
+        },
       ],
     };
   },
@@ -102,14 +102,14 @@ export default {
 </script>
 
 <style scoped>
-.container{
-    /* background-color: red; */
+.container {
+  /* background-color: red; */
 }
 .events-container {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 20px;
-   /* add some spacing between the cards */
+  /* add some spacing between the cards */
 }
 .event-card {
   /* width: 100%; */
@@ -118,7 +118,6 @@ export default {
 
 .container {
   /* background-color: aqua; */
-  
 }
 .title {
   text-align: center;
@@ -164,14 +163,12 @@ export default {
 
 /* Default styles for the cards */
 .event-card {
-  
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
   margin: 20px;
   float: left;
-    border-radius: 5px;
-  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
   transition: transform 0.2s;
   margin: 20px;
   overflow: hidden;
@@ -179,7 +176,13 @@ export default {
 }
 
 /* Media query for screens smaller than 768px */
-@media screen and (max-width: 767px) {
+@media only screen and (max-width: 600px) {
+  .events-container {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    /* grid-gap: 20px; */
+  }
+
   .event-card {
     width: 100%;
     height: 200px;
@@ -190,12 +193,12 @@ export default {
 
 /* Media query for screens between 768px and 1023px */
 @media screen and (min-width: 768px) and (max-width: 1023px) {
-  .event-card {
+  /* .event-card {
     width: calc(25% - 40px);
     height: 300px;
     margin: 20px;
     float: left;
-  }
+  } */
 }
 
 /* Media query for screens larger than 1024px */
@@ -207,6 +210,4 @@ export default {
     float: left;
   } */
 }
-
 </style>
-
