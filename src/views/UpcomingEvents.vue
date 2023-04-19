d199096ac7348f746ef6f8338a1161a7e9b51a7e
Jason Kiptoo <kiptoojason@gmail.com>
2023-04-19 09:12:07 +0300

    worked on the upcomingComponent to put words as overlay

--- src/views/UpcomingEvents.vue
+++ src/views/UpcomingEvents.vue
@@ -9,10 +9,10 @@
         <img :src="event.imageUrl" class="card-img" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{{ event.eventName }}</h5>
-          <p class="card-text">{{ event.description }}</p>
+          <!-- <p class="card-text">{{ event.description }}</p>
           <p class="card-text">Date: {{ event.date }}</p>
           <p class="card-text">Time: {{ event.time }}</p>
-          <p class="card-text">Available slots: {{ event.availableSlots }}</p>
+          <p class="card-text">Available slots: {{ event.availableSlots }}</p> -->
           <button class="btn-booknow">Book now</button>
         </div>
       </div>
@@ -32,6 +32,9 @@ export default {
           date: "2023-07-08",
           time: "12:00 PM",
           imageUrl:
+            //   "https://images.unsplash.com/photo-1605834973436-d8126806bbcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN1c3RhaW5hYmxlJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+            // "https://images.unsplash.com/photo-1556125574-d7f27ec36a06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
             "https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
           availableSlots: 500,
         },
@@ -43,19 +46,20 @@ export default {
 
 <style scoped>
 .container {
-  background-color: red;
+  /* background-color: red;
   position: relative;
   left: 0px;
-  top: 0px;
+  top: 0px; */
 }
 .events-container {
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-gap: 20px;
+  grid-gap: 20px; */
   /* add some spacing between the cards */
 }
-
-
+.event-card {
+  position: relative;
+}
 
 .title {
   text-align: center;
@@ -66,21 +70,33 @@ export default {
   transform: translateY(-5px);
 }
 .card-img {
-  height: 200px;
-  object-fit: cover;
+  display: block;
+  height: 500px;
+  object-fit: fit;
   width: 100%;
 }
 .card-body {
-  padding: 20px;
+    /* width: 100%; */
+    display: flex;
+  flex-direction: row;
+  justify-items: center;
+  align-items: flex-start;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  padding: 1rem;
 }
+
 .card-title {
   font-size: 20px;
   font-weight: bold;
-  margin-bottom: 10px;
+  margin-bottom: auto;
+  margin-top: auto;
 }
-.card-text {
-  margin-bottom: 10px;
-}
+
 .btn-booknow {
   background-color: #007bff;
   border-color: #007bff;
@@ -94,52 +110,4 @@ export default {
 }
 
 /* Default styles for the cards */
-.event-card {
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  margin: 20px;
-  float: left;
-  border-radius: 5px;
-  transition: transform 0.2s;
-  margin: 20px;
-  overflow: hidden;
-  width: 300px;
-}
-
-/* Media query for screens smaller than 768px */
-@media only screen and (max-width: 600px) {
-  .events-container {
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-    /* grid-gap: 20px; */
-  }
-
-  .event-card {
-    width: 100%;
-    height: 200px;
-    margin: 10px 0;
-    float: none;
-  }
-}
-
-/* Media query for screens between 768px and 1023px */
-@media screen and (min-width: 768px) and (max-width: 1023px) {
-  /* .event-card {
-    width: calc(25% - 40px);
-    height: 300px;
-    margin: 20px;
-    float: left;
-  } */
-}
-
-/* Media query for screens larger than 1024px */
-@media screen and (min-width: 1024px) {
-  /* .event-card {
-    width: calc(25% - 40px);
-    height: 300px;
-    margin: 20px;
-    float: left;
-  } */
-}
 </style>
